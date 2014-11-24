f74edd6d80650e1d80d6929999c2995575aca932
bmac <bmac325@GMail.com>
2014-11-24 09:26:41 -0500

    Looks like we are going to need a modified normalize function

--- app/serializers/episode.js
+++ app/serializers/episode.js
@@ -3,5 +3,14 @@ import DS from 'ember-data';
 export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
     podcast: { embedded: 'always' }
+  },
+
+  normalize: function(type, data) {
+    if (typeof data === 'number') {
+      return {
+        id: data
+      };
+    }
+    return this._super.apply(this, arguments);
   }
 });
--- app/serializers/podcast.js
+++ app/serializers/podcast.js
@@ -3,5 +3,13 @@ import DS from 'ember-data';
 export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
     episodes: { embedded: 'always' }
+  },
+  normalize: function(type, data) {
+    if (typeof data === 'number') {
+      return {
+        id: data
+      };
+    }
+    return this._super.apply(this, arguments);
   }
 });
